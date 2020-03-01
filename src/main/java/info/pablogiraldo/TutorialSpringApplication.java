6973b9010184d37d816adc57df766dc4d8d3e3c7
Pablo Giraldo <pablogirald@yahoo.es>
2020-03-01 18:23:47 +0100

    Update

--- src/main/java/info/pablogiraldo/TutorialSpringApplication.java
+++ src/main/java/info/pablogiraldo/TutorialSpringApplication.java
@@ -1,5 +1,7 @@
 package info.pablogiraldo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TutorialSpringApplication {
 
 	public static void main(String[] args) {
+//		private static final Logger logger = LoggerFactory.getLogger(TutorialSpringApplication.class);
+		final Logger log = LoggerFactory.getLogger(TutorialSpringApplication.class);
+
+		log.info("prueba info");
+		log.warn("prueba warn");
+		log.error("prueba error");
+		log.debug("prueba debug");
+
 		SpringApplication.run(TutorialSpringApplication.class, args);
 
 	}
--- src/main/java/info/pablogiraldo/controlador/MiControlador.java
+++ src/main/java/info/pablogiraldo/controlador/MiControlador.java
@@ -2,6 +2,8 @@ package info.pablogiraldo.controlador;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,14 @@ public class MiControlador {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+	@Autowired
+	Environment env;
+
 	@RequestMapping(value = "")
 	public String hola(ModelMap mp) {
+		String[] envStr = env.getActiveProfiles();
 		mp.put("textoHola", "Hola Mundo");
-		log.debug("prueba");
+		log.info(envStr[0]);
 		return "HolaMundo";
 	}
 
